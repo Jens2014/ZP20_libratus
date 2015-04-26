7bc0dd333912a0ea146c7409625b9e1ad3900051
--- <---@Home2.fritz.box>
2015-04-26 22:08:17 +0200

    ver 0.9.1.2

--- libratus/theme_description.php
+++ libratus/theme_description.php
@@ -3,5 +3,5 @@ $theme_description['name'] = 'Libratus';
 $theme_description['author'] = 'JeKoPhoto - jekophoto.eu';
 $theme_description['version'] = '0.9.1.2';
 $theme_description['date'] = '15/4/26';
-$theme_description['desc'] = 'A Zenphoto20/Zenpage theme with many new features focused on statistics (latest, ratings, popular, hits).';
+$theme_description['desc'] = 'A Zenphoto20/Zenpage responsive theme with many new features focused on statistics (latest, ratings, popular, hits).';
 ?>
